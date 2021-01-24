b7dffb569dd404b39e6e663eb49c0e97bc9f7108
AlexNexton <cianoc13@gmail.com>
2021-01-24 13:32:03 +0000

    first video: added sites to def_init_ funtion

--- scraper.py
+++ scraper.py
@@ -1,7 +1,16 @@
 class JobScrape:
 
-  def __init__():
-    pass
+  def __init__(self, sit_name):
+    """Having the job sites in a list means
+    we can check on initialisation and throw an error
+    if the site is unavailable"""
+    
+    sites = [{"monster": {
+      "url":"https://www.monster.ie/jobs/search/",
+      "query_format":"?q={keywords}&where={city}&cy={country}",
+      "results": "#ResultsContainer",
+    }}]
+
 
   def _format_monster():
     pass
