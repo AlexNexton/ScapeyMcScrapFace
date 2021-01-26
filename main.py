4b27a9345ebe5391c978b6d54e86215ef6038eba
AlexNexton <cianoc13@gmail.com>
2021-01-26 11:58:55 +0000

    video three finished

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from scraper import JobScrape
 
 mon = JobScrape("monster")
+
+mon_results = mon.get_jobs("dublin", "ie", "fzzkkzzeer")
--- scraper.py
+++ scraper.py
@@ -7,15 +7,15 @@ class JobScrape:
     we can check on initialisation and throw an error
     if the site is unavailable"""
     
-    sites = [{"monster" : {
+    sites = [{"monster": {
         "url":"https://www.monster.ie/jobs/search/",
-        "query_format" : "?q={keywords}&where={city}&cy={country}",
-        "results" : "#ResultsContainer",
+        "query_format":"?q={keywords}&where={city}&cy={country}",
+        "results": "#ResultsContainer",
+        "not_found" : ".pivot.block",
       }}]
     try:
-        self.site_data = [site[site_name] for site in sites if site_name in site]
-        [0]
-        self.site_name = site_name
+      self.site_data = [site[site_name] for site in sites if site_name in site][0]
+      self.site_name = site_name
        
     except IndexError:
       raise ValueError(f"{site_name} is not found or supported yet")
@@ -39,13 +39,17 @@ class JobScrape:
 
     r = s.get(f"{base_url}{query}")
 
-    return r.html.find(self.site_data["results"], first=True)
+    if r.html.find(self.site_data["not_found"]):
+      return None
+    else:
+      return r.html.find(self.site_data["results"], first=True)
 
   def get_jobs(self, city,country,keywords, desc=True):
     """Method of the class. Calls the scraper and formats
     the results based on which site was selected"""
 
-    jobs = self._scrape_site(city,country,keywords)
+    jobs = self._scrape_site(city, country, keywords)
+
 
     if self.site_name.lower() == "monster":
         return self._format_monster(jobs, desc) if jobs else None
